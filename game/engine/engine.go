89d6dc843067a7e69d5effdde876237e5f7a0026
dbrajovic <dbrajovic3@gmail.com>
2022-10-27 23:48:15 +0200

    engine: add stubs

--- game/engine/engine.go
+++ game/engine/engine.go
@@ -3,8 +3,22 @@ package engine
 import "github.com/alien_invasion/game/types"
 
 type Engine struct {
+	aliens  []types.Alien
+	gameMap Map
 }
 
 func New(aliens []types.Alien, gameMap Map) *Engine {
-	return nil
+	return &Engine{aliens, gameMap}
+}
+
+func (e *Engine) Run() {
+	for {
+		//	check if game should stop
+
+		//	move aliens
+
+		//	fight aliens
+
+		//	remove destroyed cities (if any)
+	}
 }
