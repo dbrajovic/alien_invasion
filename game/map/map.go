6f3a826b54f2c6cc3b38c9236e6a5f8ea53bbdc4
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 01:30:10 +0200

    create map package and define stubs

---
+++ game/map/map.go
@@ -0,0 +1,26 @@
+package _map
+
+import "github.com/alien_invasion/game/types"
+
+type Map struct {
+}
+
+func New(filename string) *Map {
+	return &Map{}
+}
+
+func (m *Map) Display() {
+
+}
+
+func (m *Map) Cities() []types.City {
+	return nil
+}
+
+func (m *Map) RandomNeighbourCity(city types.City) types.City {
+	return ""
+}
+
+func (m *Map) RemoveCity(city types.City) {
+
+}
