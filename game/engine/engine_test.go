44ddbc404a5d84f2c8bd4539b794ea2746e6b2fc
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 00:06:49 +0200

    engine: add test

--- game/engine/engine_test.go
+++ game/engine/engine_test.go
@@ -1,7 +1,53 @@
 package engine
 
-import "testing"
+import (
+	"testing"
 
-func TestEngine_Run(t *testing.T) {
+	"github.com/alien_invasion/game/types"
+	"github.com/stretchr/testify/assert"
+)
 
+func TestEngine_isDone(t *testing.T) {
+	t.Run(
+		"aliens are maxed out on travels",
+		func(t *testing.T) {
+			e := New(
+				[]types.Alien{
+					{
+						Name:     "alien_1",
+						Location: "belgrade",
+						Steps:    maxTravels},
+				},
+				nil,
+			)
+
+			assert.True(t, e.isDone())
+		},
+	)
+
+	t.Run(
+		"aliens are dead",
+		func(t *testing.T) {
+			e := New(
+				[]types.Alien{},
+				nil,
+			)
+
+			assert.True(t, e.isDone())
+		},
+	)
+
+	t.Run(
+		"aliens are not maxed out on travels",
+		func(t *testing.T) {
+			e := New(
+				[]types.Alien{
+					{Name: "alien", Location: "belgrade", Steps: 0},
+				},
+				nil,
+			)
+
+			assert.False(t, e.isDone())
+		},
+	)
 }
