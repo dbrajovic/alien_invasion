173d35c82b9e15cd8ae0c68949c44930dd7bb49f
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 00:13:43 +0200

    engine: implement isDone

--- game/engine/engine.go
+++ game/engine/engine.go
@@ -2,6 +2,10 @@ package engine
 
 import "github.com/alien_invasion/game/types"
 
+const (
+	maxTravels uint64 = 10000
+)
+
 type Engine struct {
 	aliens  []types.Alien
 	gameMap Map
@@ -14,6 +18,9 @@ func New(aliens []types.Alien, gameMap Map) *Engine {
 func (e *Engine) Run() {
 	for {
 		//	check if game should stop
+		if e.isDone() {
+			return
+		}
 
 		//	move aliens
 
@@ -21,4 +28,22 @@ func (e *Engine) Run() {
 
 		//	remove destroyed cities (if any)
 	}
+}
+
+func (e *Engine) isDone() bool {
+	return e.areAliensDead() || e.areAliensMaxedOutOnTravels()
+}
+
+func (e *Engine) areAliensDead() bool {
+	return len(e.aliens) == 0
+}
+
+func (e *Engine) areAliensMaxedOutOnTravels() bool {
+	for _, alien := range e.aliens {
+		if alien.Steps < maxTravels {
+			return false
+		}
+	}
+
+	return true
 }
