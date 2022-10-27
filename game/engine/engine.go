145fe1f9a713163c6b61ae93485700e890c148b7
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 01:05:30 +0200

    engine: implement aliensFight

--- game/engine/engine.go
+++ game/engine/engine.go
@@ -26,6 +26,7 @@ func (e *Engine) Run() {
 		e.moveAliens()
 
 		//	fight aliens
+		e.aliensFight()
 
 		//	remove destroyed cities (if any)
 	}
@@ -58,4 +59,38 @@ func (e *Engine) moveAliens() {
 
 func (e *Engine) move(alien types.Alien) {
 
+}
+
+func (e *Engine) aliensFight() []types.City {
+
+	aliensByCities := make(map[types.City][]*types.Alien)
+	for _, alien := range e.aliens {
+		existing := aliensByCities[alien.Location]
+		existing = append(existing, alien)
+		aliensByCities[alien.Location] = existing
+	}
+
+	var (
+		destroyedCities []types.City
+		deadAliens      []*types.Alien
+	)
+
+	for city, aliens := range aliensByCities {
+		if len(aliens) > 1 {
+			destroyedCities = append(destroyedCities, city)
+			deadAliens = append(deadAliens, aliens...)
+
+			//	print
+		}
+	}
+
+	for _, a := range deadAliens {
+		for i, aa := range e.aliens {
+			if a.Name == aa.Name {
+				e.aliens = append(e.aliens[:i], e.aliens[i+1:]...)
+			}
+		}
+	}
+
+	return destroyedCities
 }
