fd427f729fb9b6cd1b8c6b986ee96ca97d581ab8
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 02:32:47 +0200

    game: implement ctor

--- game/game.go
+++ game/game.go
@@ -1,12 +1,22 @@
 package game
 
+import (
+	"github.com/alien_invasion/game/engine"
+	_map "github.com/alien_invasion/game/map"
+)
+
+const (
+	mapFilename string = "game_map"
+)
+
 type Game struct {
-	//	engine
+	*engine.Engine
 }
 
-func New() *Game {
-	println("new game created")
-	return nil
+func New(numAliens uint64) *Game {
+	return &Game{
+		engine.New(numAliens, _map.New(mapFilename)),
+	}
 }
 
 func (g *Game) Run() {
