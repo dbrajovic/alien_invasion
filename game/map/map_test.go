0d9d022c68ae8a0aa8d63df6c4e2894eb54587a6
dbrajovic <dbrajovic3@gmail.com>
2022-10-28 02:16:03 +0200

    map: add test case

--- game/map/map_test.go
+++ game/map/map_test.go
@@ -7,26 +7,54 @@ import (
 )
 
 func TestMap_RemoveCity(t *testing.T) {
-	m := &Map{cities: map[types.City]*neighbourhood{
-		"belgrade": {
-			west: "dubrovnik",
+	t.Run(
+		"middle city is removed",
+		func(t *testing.T) {
+			m := &Map{cities: map[types.City]*neighbourhood{
+				"belgrade": {
+					west: "dubrovnik",
+				},
+
+				"dubrovnik": {
+					east:  "belgrade",
+					north: "berlin",
+				},
+
+				"berlin": {
+					south: "dubrovnik",
+				},
+			}}
+
+			m.RemoveCity("dubrovnik")
+
+			assert.Len(t, m.cities, 2)
+			assert.NotContains(t, m.cities, "dubrovnik")
+
+			assert.False(t, m.cities["belgrade"].isNeighbour("dubrovnik"))
+			assert.False(t, m.cities["berlin"].isNeighbour("dubrovnik"))
 		},
+	)
 
-		"dubrovnik": {
-			east:  "belgrade",
-			north: "berlin",
+	t.Run(
+		"left city is removed",
+		func(t *testing.T) {
+			m := &Map{cities: map[types.City]*neighbourhood{
+				"belgrade": {
+					west: "dubrovnik",
+				},
+
+				"dubrovnik": {
+					east: "belgrade",
+				},
+			}}
+
+			m.RemoveCity("dubrovnik")
+
+			assert.Len(t, m.cities, 1)
+			assert.NotContains(t, m.cities, "dubrovnik")
+
+			assert.False(t, m.cities["belgrade"].isNeighbour("dubrovnik"))
 		},
+	)
 
-		"berlin": {
-			south: "dubrovnik",
-		},
-	}}
-
-	m.RemoveCity("dubrovnik")
-
-	assert.Len(t, m.cities, 2)
-	assert.NotContains(t, m.cities, "dubrovnik")
-
-	assert.False(t, m.cities["belgrade"].isNeighbour("dubrovnik"))
-	assert.False(t, m.cities["berlin"].isNeighbour("dubrovnik"))
 }
